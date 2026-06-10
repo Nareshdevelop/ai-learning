34de4ad4331a82d828fa249a058582c39fd08678
akhila <akhilachamarthi1995@gmail.com>
2026-06-10 12:34:37 -0500

    composition

---
+++ phase0/composition.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+a = np.array([[0, -1], [1, 0]])
+b = np.array([[1,  1], [0, 1]]) 
+v = np.array([2, 1])
+
+transform1 = b @ v
+
+transform2 = a @ transform1 
+
+print("step by step:", transform2)
+
+print("A @ B @ v :", (a @ b) @ v)    # shear, then rotate
+print("B @ A @ v :", (b @ a) @ v)    # rotate, then shear
